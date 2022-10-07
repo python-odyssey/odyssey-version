ac124a3ac91097b37f2466d2838d9eeeccfd312f
Justin Sharma <justin.elite@gmail.com>
2022-10-06 22:16:44 -0700

    chore: support type checking, linting, and coverage as part of running pytest

--- src/odyssey/version/__init__.py
+++ src/odyssey/version/__init__.py
@@ -1,2 +1,22 @@
-def example():
+from enum import Enum, unique, auto
+
+
+@unique
+class VersionType(Enum):
+    PEP440 = auto()
+    SemVer2_0 = auto()
+
+
+class Version:
+    def __init__(self, text: str = "1.0.0") -> None:
+        self.text = text
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, self.__class__):
+            return self.text == other.text
+
+        return False
+
+
+def example() -> str:
     return "Hello, World!"
--- src/odyssey_tests/version_tests/__init__.py
+++ src/odyssey_tests/version_tests/__init__.py
@@ -1,9 +1,29 @@
-from odyssey import version
+from odyssey.version import example, Version
 
 
-def test_example():
+def test_example() -> None:
     expected = "Hello, World!"
 
-    result = version.example()
+    result = example()
 
     assert expected == result
+
+
+def test_Version_eq_Version() -> None:
+    expected = Version("1.0.0")
+
+    result = Version()
+
+    assert expected == result
+
+
+def test_Version_eq_str() -> None:
+    value = "1.0.0"
+
+    result = Version()
+
+    # Uses str __eq__
+    assert not value == result
+
+    # Uses Version __eq__
+    assert not result == value
